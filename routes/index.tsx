c29b2b4e00083038795ea01014ed6093f7b2af7f
francois plet <f.plet@jurisoft.fr>
2022-11-09 10:20:44 +0100

    scaffold

---
+++ routes/index.tsx
@@ -0,0 +1,14 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ma Fresh App</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        Hello World
+      </div>
+    </>
+  );
+}
