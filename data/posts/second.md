5c3c3b16c002cb45782095e21402ee85793ff429
francois plet <francisco_canari@hotmail.fr>
2022-11-09 11:56:36 +0100

    add load post feature

--- data/posts/second.md
+++ data/posts/second.md
@@ -4,6 +4,8 @@ publish_at: 2022-11-10T10:09:40.692Z
 snippet: this is the second post
 ---
 
+## Subtitle
+
 table
 
 | Table | Column |
--- utils/Post.ts
+++ utils/Post.ts
@@ -1,7 +1,34 @@
+import { extract } from "$std/encoding/front_matter.ts";
+
 export interface Post {
     id: string;
     title: string;
     publishedAt: Date;
     snippet: string;
     content: string;
+}
+
+export async function load(id: string): Promise<Post | null> {
+    let text: string;
+    try {
+        text = await Deno.readTextFile(`./data/posts/${id}.md`)
+    } catch (error) {
+        if(error instanceof Deno.errors.NotFound)
+        {
+            return null;
+        }
+        return error;
+    }
+
+    const {attrs, body} = extract(text);
+    const params = attrs as Record<string, string>;
+    const publishedAt = new Date(params.publish_at);
+
+    return {
+        id,
+        title: params.title,
+        publishedAt,
+        snippet: params.snippet,
+        content: body
+    };
 }
